ed4de947fa1627260afebfcf829fc263225e43d3
Borshadevi <borshadevi27@gmail.com>
2024-06-04 00:53:57 +0600

    add login data

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,15 +1,16 @@
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
         <div>
-          <div className="hero min-h-screen bg-base-200">
+          <div className="hero min-h-screen ">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <img src="https://i.ibb.co/Fqx9VRJ/20602920-6325227.jpg" className="max-w-screen-sm " alt="" />
+    <div className="card shrink-0 w-full max-w-screen-sm shadow-2xl bg-base-100">
+        <p className=" border border-teal-500 p-3 hover:border-teal-300 ml-20 mr-20 flex justify-center items-center"><FcGoogle /><span className="font-bold text-xl ml-4">GOOGLE</span></p>
+        <div className="divider">OR Email and password </div>
       <form className="card-body">
         <div className="form-control">
           <label className="label">
@@ -27,9 +28,10 @@ const Login = () => {
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn bg-teal-400">Login</button>
         </div>
       </form>
+      <p className="p-4 text-center">Do not have any account?<Link to='/signUp' className="font-bold  text-teal-600"><span>Sign Up</span></Link></p>
     </div>
   </div>
 </div>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,39 +1,41 @@
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 
 const SignUp = () => {
     return (
         <div>
-         <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
+        <div className="hero min-h-screen ">
+<div className="hero-content flex-col lg:flex-row-reverse">
+  <img src="https://i.ibb.co/yqHsL0p/4957412-Mobile-login-Cristina.jpg" className="max-w-screen-sm " alt="" />
+  <div className="card shrink-0 w-full max-w-screen-sm shadow-2xl bg-base-100">
+      <p className=" border border-teal-500 p-3 hover:border-teal-300 ml-20 mr-20 flex justify-center items-center"><FcGoogle /><span className="font-bold text-xl ml-4">GOOGLE</span></p>
+      <div className="divider">OR Email and password </div>
+    <form className="card-body">
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Email</span>
+        </label>
+        <input type="email" placeholder="email" className="input input-bordered" required />
+      </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Password</span>
+        </label>
+        <input type="password" placeholder="password" className="input input-bordered" required />
+        <label className="label">
+          <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+        </label>
+      </div>
+      <div className="form-control mt-6">
+        <button className="btn bg-teal-400">Sign Up</button>
+      </div>
+    </form>
+    <p className="p-4 text-center">Already have account?<Link to='/login' className="font-bold  text-teal-600"><span>Login</span></Link></p>
   </div>
 </div>
-        </div>
+</div>
+      </div>
     );
 };
 
