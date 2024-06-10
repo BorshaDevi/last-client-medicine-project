0d4058bc4099e7b874f9a1294e89c19029e92254
Borshadevi <borshadevi27@gmail.com>
2024-06-10 23:57:07 +0600

    add text

--- src/Pages/DashBoradPage/AdminDashBorad/AdminDashBorad.jsx
+++ src/Pages/DashBoradPage/AdminDashBorad/AdminDashBorad.jsx
@@ -6,30 +6,13 @@ import { RiSecurePaymentFill } from "react-icons/ri";
 import { FcSalesPerformance } from "react-icons/fc";
 import { FcAdvertising } from "react-icons/fc";
 
+
 const AdminDashBorad = () => {
     const {user}=useAuth()
+   
     return (
         <div className="">
-            <div className="stats shadow ">
-  
-  <div className="stat">
-    <div className="stat-figure text-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-    </div>
-    <div className="stat-title">Total Likes</div>
-    <div className="stat-value text-primary">25.6K</div>
-    <div className="stat-desc">21% more than last month</div>
-  </div>
-  
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-    </div>
-    <div className="stat-title">Page Views</div>
-    <div className="stat-value text-secondary">2.6M</div>
-    <div className="stat-desc">21% more than last month</div>
-  </div>
-    </div>
+         
              <aside className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
     <a href="#">
         <img className="max-w-scree-full h-6 sm:h-7" src="https://i.ibb.co/VgGN3mf/31560524-14.jpg" alt=""/>
